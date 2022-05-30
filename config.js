0b70bf5746f2ece7ec89c9d74899f48865b7f078
unknown <ngtrang682@gmail.com>
2022-05-30 22:53:47 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo bẹn thúi nhà tui ! 🖐🖐🖐',
+    introDesc: `Mintren có điều này muốn nói với em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Uki nè',
+    title: 'Em iu mintren nhở ? 😙',
+    desc: 'Trụttttttttttt iu nắmmmm ý :> ',
+    btnYes: 'Iu nhìu nắmmmmmm bằng vú trụuuu 💖💖💖',
+    btnNo: 'Không hề nha cái con mông to! 💔 ',
+    question:'Mintren iu em nhìu nắm. Còn bẹn thúi 😊😊',
+    reasonPlaceholder: 'Tuiii cũng iu bẹn mintren nhìu nắm',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Hy hy dạaa iuuuu emmmm 😊😊',
+    mess: 'Mintren thítttt bạn Khánh Linh Thúi nhìu nắmmm 🥰. Trụtttt trụtttt trụttt 😘😘',
+    messDesc: '💬Tắm sớm iii naaaa. 💬💬',
+    btnAccept: 'Dạ iu iuuuuu 💖',
+    messLink: 'https://www.vinmec.com/vi/tin-tuc/thong-tin-suc-khoe/song-khoe/tam-khuya-co-thuc-su-lien-quan-den-dot-quy/?link_type=related_posts#:~:text=T%E1%BA%AFm%20khuya%20%E1%BA%A3nh%20g%C3%A2y%20nh%E1%BB%AFng,v%E1%BB%87%20s%E1%BB%A9c%20kh%E1%BB%8Fe%20c%C6%A1%20th%E1%BB%83.'
+}
